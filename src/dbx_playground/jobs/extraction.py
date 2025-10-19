7a24abacc7ae5cef13387ec5e2ba6d502341e93d
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-10-19 21:05:02 +0100

    feat: add dataset extraction job

---
+++ src/dbx_playground/jobs/extraction.py
@@ -0,0 +1,46 @@
+import typing as T
+from pathlib import Path
+
+import kaggle
+import polars as pl
+
+import dbx_playground.jobs.base as base
+
+
+class ExtractionJob(base.Job):
+    KIND: T.Literal["ExtractionJob"] = "ExtractionJob"
+
+    path: str
+    dataset: str
+    unzip: bool
+
+    @T.override
+    def run(self) -> base.Locals:
+        kaggle.api.authenticate()
+
+        output_dir = Path(self.path).parent
+        Path(output_dir).mkdir(exist_ok=True, parents=True)
+
+        kaggle.api.dataset_download_files(
+            dataset=self.dataset, path=output_dir, unzip=self.unzip
+        )
+
+        paths = Path(output_dir).glob("*.csv")
+
+        datasets = []
+        for path in paths:
+            if not Path(path).exists():
+                raise FileNotFoundError(f"Data did not download correctly: {path}")
+
+            data = pl.read_csv(path)
+
+            # delete original csv files
+            path.unlink()
+
+            datasets.append(data)
+
+        # concat datasets together
+        df: pl.DataFrame = pl.concat(datasets, how="vertical_relaxed")
+        df.write_parquet(self.path)
+
+        return locals()
