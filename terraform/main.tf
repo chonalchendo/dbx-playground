4e4cdcb94dbc88e9822c1379b42cba0198f90321
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-10-20 21:02:28 +0100

    feat: refactor Databricks and AWS module configurations for improved structure

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,3 @@
-provider "databricks" {
-  host  = var.hostname
-  token = var.pat_token
-}
 
 data "databricks_current_user" "me" {}
 data "databricks_spark_version" "latest" {}
@@ -9,48 +5,36 @@ data "databricks_node_type" "smallest" {
   local_disk = true
 }
 
-resource "databricks_notebook" "this" {
-  path     = "${data.databricks_current_user.me.home}/Terraform"
-  language = "PYTHON"
-  content_base64 = base64encode(<<-EOT
-    # created from ${abspath(path.module)}
-    display(spark.range(10))
-    EOT
-  )
+# databricks modules
+module "notebook" {
+  source = "./modules/databricks/notebooks"
+  home = data.databricks_current_user.me.home
 }
 
-resource "databricks_notebook" "test" {
-  source = "../notebooks/test.ipynb"
-  path   = "${data.databricks_current_user.me.home}/test"
-}
-
-resource "databricks_notebook" "test_2" {
-  source = "../notebooks/test_2.py"
-  path   = "${data.databricks_current_user.me.home}/test_2"
-}
-
-resource "databricks_schema" "sandbox" {
+module "schema" {
+  source = "./modules/databricks/schemas"
   catalog_name = "workspace"
-  name         = "sandbox"
-  comment      = "this database is managed by terraform"
-  properties = {
-    kind = "various"
-  }
 }
 
-resource "databricks_schema" "prod" {
-  catalog_name = "workspace"
-  name         = "prod"
-  comment      = "this database is managed by terraform"
-  properties = {
-    kind = "various"
-  }
+module "dbx_playground_library" {
+  source        = "./modules/databricks/libraries"
+  catalog_name  = "workspace"
+  schema_name   = "prod"
+  volume_name   = "volume"
+  whl_local_path = "${path.module}/../dist/dbx_playground-0.1.0-py3-none-any.whl"
 }
 
-output "notebook_url" {
-  value = databricks_notebook.this.url
+# aws modules
+module "s3" {
+  source = "./modules/aws/s3"
+  project_name = var.project_name
+  project_bucket_name = var.bucket_name
 }
 
-output "databricks_schema_id" {
-  value = databricks_schema.sandbox.id
+module "iam" {
+  source = "./modules/aws/iam"
+  project_name      = var.project_name
+  project_user_name = var.project_name
+  # Bucket ARNs from S3 module
+  project_bucket_arn = module.s3.project_bucket_arn
 }
