8a25513b45fbecf222684494652a93c3c3f233b6
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-10-20 21:02:20 +0100

    feat: update provider configuration for AWS and Databricks

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,5 +3,24 @@ terraform {
     databricks = {
       source = "databricks/databricks"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0.0"
+    }
+  }
+}
+
+provider "databricks" {
+  host  = var.hostname
+  token = var.pat_token
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = var.project_name
+    }
   }
 }
