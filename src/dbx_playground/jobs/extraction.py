420e92abb0dca99cd2f5dbd968e1ee78e9d30bcc
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-10-23 20:36:12 +0100

    feat: add logging to extraction job for more information

--- src/dbx_playground/jobs/extraction.py
+++ src/dbx_playground/jobs/extraction.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import kaggle
 import polars as pl
 from dotenv import load_dotenv
+from loguru import logger
 
 import dbx_playground.jobs.base as base
 
@@ -19,16 +20,20 @@ class ExtractionJob(base.Job):
 
     @T.override
     def run(self) -> base.Locals:
+        logger.info("Authenticating Kaggle API")
         kaggle.api.authenticate()
 
         output_dir = Path(self.path).parent
 
+        logger.info("Creating Tmp folder")
         tmp_folder = Path("tmp")
         tmp_folder.mkdir(exist_ok=True)
 
         if "s3" not in self.path:
+            logger.info(f"Creating output directory: {output_dir}")
             output_dir.mkdir(exist_ok=True, parents=True)
 
+        logger.info(f"Downloading dataset: {self.dataset}")
         kaggle.api.dataset_download_files(
             dataset=self.dataset, path=tmp_folder, unzip=self.unzip
         )
@@ -38,13 +43,12 @@ class ExtractionJob(base.Job):
         datasets = []
         for path in paths:
             if not Path(path).exists():
+                logger.error(f"Data did not download correctly: {path}")
                 raise FileNotFoundError(f"Data did not download correctly: {path}")
 
             data = pl.read_csv(path)
-
             # delete original csv files
             path.unlink()
-
             datasets.append(data)
 
         # delete tmp folder
@@ -52,6 +56,9 @@ class ExtractionJob(base.Job):
 
         # concat datasets together
         df: pl.DataFrame = pl.concat(datasets, how="vertical_relaxed")
+
+        logger.info(f"Writing dataset to path: {self.path}")
         df.write_parquet(self.path)
 
+        logger.success("Successfully completed ExtractionJob")
         return locals()
